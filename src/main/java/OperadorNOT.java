39119cbef1bb6eca6ee4d89403a4a690fa9c384a
m4rcous <m.gongora.sanchez08@gmail.com>
2025-01-11 22:16:34 -0500

    feat: learn about operator NOT in Java

---
+++ src/main/java/OperadorNOT.java
@@ -0,0 +1,8 @@
+public class OperadorNOT {
+    public static void main(String[] args) {
+        System.out.println("*** Operador NOT ***");
+        boolean a = false;
+        // Operador NOT ! (invertir el valor lógico)
+        var resultado = !a;
+    }
+}
