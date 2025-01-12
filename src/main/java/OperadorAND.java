14101b80ba5f50c201e9cdea1f5111288627b423
m4rcous <m.gongora.sanchez08@gmail.com>
2025-01-11 22:16:03 -0500

    feat: learn about operator AND in Java

---
+++ src/main/java/OperadorAND.java
@@ -0,0 +1,9 @@
+public class OperadorAND {
+    public static void main(String[] args) {
+        System.out.println("*** Operador AND ***");
+        boolean a = true, b = false;
+        // AND (regresa true si ambos son verdaderos)
+        var resultado = a && b;
+        System.out.println("resultado = " + resultado);
+    }
+}
